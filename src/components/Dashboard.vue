f2306e1c3744ddb3c7491200de616c17052c1bca
andreenakashima <andreenakashima@gmail.com>
2022-07-04 11:07:43 -0300

    Adicionado update do status do pedido

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -22,13 +22,13 @@
                         </ul>
                     </div>
                     <div>
-                        <select name="status" class="status">
+                        <select name="status" class="status" @change="updatedBurger($event, burger.id)">
                             <option value="">Selecione</option>
-                            <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo">
+                            <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burger.status == s.tipo">
                                 {{ s.tipo }}
                             </option>
                         </select>
-                        <button class="delete-btn">Cancelar</button>
+                        <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
                     </div>
                 </div>
             </div>
@@ -64,6 +64,27 @@
                 
                 this.status = data;
 
+            },
+            async deleteBurger(id) {
+                const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                    method: "DELETE"
+                });
+
+                const res = await req.json();
+
+                this.getPedidos();
+            },
+            async updatedBurger(event, id) {
+                const option = event.target.value;
+                const dataJson = JSON.stringify({ status: option });
+
+                const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                    method: "PATCH",
+                    headers: { "Content-Type":"application/json" },
+                    body: dataJson
+                });
+
+                const res = await req.json();
             }
         },
         mounted() {
